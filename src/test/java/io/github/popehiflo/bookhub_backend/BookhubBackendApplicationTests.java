5ffcd2b55e8698bfe0e11e56533f2da740a1ef70
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2023-10-15 11:06:55 -0500

    feat: initialize project (backend) with spring initialzr

---
+++ src/test/java/io/github/popehiflo/bookhub_backend/BookhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.popehiflo.bookhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
